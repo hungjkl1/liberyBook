1519cb0c6716536c5a2db0820d4ad4bbc223d036
hungjkl1 <hungjkl1@gmail.com>
2019-12-05 23:56:01 +0700

    first init

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,20 @@
+﻿using Library.Repository.Repositories;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Library.Service.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    public class BooksController : Controller
+    {
+        private BooksRepository _booksRepository = new BooksRepository();
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(JsonConvert.SerializeObject(_booksRepository.GetAllBooks()));
+        }
+    }
+}
